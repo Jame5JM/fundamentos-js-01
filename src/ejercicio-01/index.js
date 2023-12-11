4a34a555d3d1aafaea195be6f8d77890a21631fb
Jame5JM <jamesjmann@gmail.com>
2023-12-11 01:49:49 +0100

    Anadir el ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,25 @@
+const nombre = 'James'
+
+const edad = 45
+
+const esEstudiante = true
+
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años y soy estudiante: '  + esEstudiante + '.'
+
+const num1 = 15
+
+const num2 = 5
+
+const suma = num1 + num2 
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const division = num1 / num2
+
+console.log(mensaje)
+console.log('Suma:',  suma)
+console.log('Resta:', resta)
+console.log('Multiplacicion:', multiplicacion)
+console.log('Division:', division)
+
+
+
