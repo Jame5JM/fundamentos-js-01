7acc5043ca2a766e05e738db0ad330ea0f176324
Jame5JM <jamesjmann@gmail.com>
2023-12-12 22:15:03 +0100

    Añadir ejercicio 3

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,16 @@
+// Ejercicio 3: Operador Ternario y Tipos de Datos
+
+// 1. Variable `numero`.
+const numero = 22
+
+// 2. Operador ternario para asignar `tipoNumero`.
+const tipoNumero = numero > 0 ? 'positivo' : 'negativo o cero'
+console.log('El número es ' + tipoNumero)
+
+// 3. Variable `cadena`.
+const cadena = 'Hola Mundo'
+
+// 4. Operadotr ternario para asignar `longtitudCadena`.
+const longtitudCadena = cadena.length > 5 ? 'larga' : 'corta'
+console.log('La cadena es ' + longtitudCadena)
+
